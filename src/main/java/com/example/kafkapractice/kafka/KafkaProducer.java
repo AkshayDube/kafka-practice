e1d1d7bfaa94cb6176ed3824c93d65d9c39cc4a8
AkshayDube <dubeyakshay09@gmail.com>
2024-05-15 21:09:24 +0530

    Replaced println with logger.

--- src/main/java/com/example/kafkapractice/kafka/KafkaProducer.java
+++ src/main/java/com/example/kafkapractice/kafka/KafkaProducer.java
@@ -1,5 +1,7 @@
 package com.example.kafkapractice.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -7,12 +9,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaProducer {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("Second-Topic",message);
-        System.out.println("Message Sent is : " + message);
+        kafkaTemplate.send("Second-Topic", message);
+        log.info("Message Sent is : " + message);
     }
 
 }
